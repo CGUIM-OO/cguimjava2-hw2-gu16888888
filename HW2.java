af2bf6e6496d1162b76902c32e6903dfb6279007
gu16888888 <943223755@qq.com>
2017-10-29 20:39:07 +0800

    HW2

--- HW2.java
+++ HW2.java
@@ -1,10 +1,12 @@
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * @author TODO: please add student ID and name here
+ * @author TODO: please add student ID and name here B0544201 ¹ËÓêŞ±
  * Try to write some comments for your codes (methods, 15 points)
+ * ÏÈÈ·¶¨ÊäÈëÓĞ¼¸¸±ÅÆ£¨sc£©£¬½«deck¡¢cardÊµÌå»¯£¬ÓÃÈı¸öfor»ØÈ¦µÄ·½Ê½Ñ­»··Ö±ğĞ´³ö»¨É«¡¢´óĞ¡¡¢sc¸±ÅÆ²¢Í¨¹ıif print³öÀ´
  */
 public class HW2 {
 
@@ -26,8 +28,8 @@ public class HW2 {
 	}
 	/**
 	 * This method is used for checking your result, not a part of your HW2
-	 * @param allCards æ‰€æœ‰çš„ç‰Œ
-	 * @param nDeck ç¸½å…±æœ‰å¹¾å‰¯ç‰Œ
+	 * @param allCards ËùÓĞµÄÅÆ
+	 * @param nDeck ¿‚¹²ÓĞ×¸±ÅÆ
 	 * @return
 	 */
 	private static boolean isAllCardsCorrect(ArrayList<Card> allCards,int nDeck){
@@ -43,7 +45,7 @@ public class HW2 {
 			}
 			if(checkHash.containsKey(suit+","+rank)){
 				checkHash.put(suit+","+rank, 
-						checkHash.get(suit+","+rank)+1);
+						checkHash.get(suit+","+rank)+1);                                                                                                                              
 			}else{
 				checkHash.put(suit+","+rank, 1);
 			}
@@ -65,6 +67,7 @@ public class HW2 {
 }
 /**
  * Description: TODO: please add description here
+ * µÚÒ»¸ö»ØÈ¦´ú±íÓĞ¼¸¸±ÅÆ£¬µÚ¶ş¸ö»ØÈ¦´ú±í»¨É«£¬µÚÈı¸ö»ØÈ¦´ú±í´óĞ¡
  */
 class Deck{
 	private ArrayList<Card> cards;
@@ -77,13 +80,27 @@ class Deck{
 		//Card card=new Card(1,1); ->means new card as clubs ace
 		//cards.add(card);
 		//Sample code end
+		for (int deck=1;deck<=nDeck;deck++){//¼¸¸±ÅÆ
+			for(int suit=1;suit<=4;suit++){//»¨É«
+				for(int Rank=1;Rank<=13;Rank++){//´óĞ¡
+					Card card=new Card(suit,Rank);
+					cards.add(card);//½«ĞÂÊµÌå»¯µÄcard¼Óµ½Çåµ¥£¨cards£©ÀïÃæ
+				}
+			}
+		}
+		
 
 	}	
 	//TODO: Please implement the method to print all cards on screen (10 points)
 	public void printDeck(){
 		//Hint: print all items in ArrayList<Card> cards, 
 		//TODO: please implement and reuse printCard method in Card class (5 points)
-
+		int n=0;
+		while (cards.size()>n){
+			Card card=new Card(cards.get(n).getSuit(),cards.get(n).getRank());
+			card.printCard();//ºô½Ğprintcard
+			n++;
+		}
 	}
 	public ArrayList<Card> getAllCards(){
 		return cards;
@@ -91,6 +108,8 @@ class Deck{
 }
 /**
  * Description: TODO: please add description here
+ * ½«»¨É«¼°´óĞ¡±àºÅ£¨Clubs=1, Diamonds=2, Hearts=3, Spades=4£©£¨rank 1-13£©
+ * Í¨¹ıif ÅĞ¶ÏÏàÓ¦µÄ»¨É«ºÍ´óĞ¡²¢print³öÀ´
  */
 class Card{
 	private int suit; //Definition: 1~4, Clubs=1, Diamonds=2, Hearts=3, Spades=4
@@ -106,7 +125,41 @@ class Card{
 	//TODO: 1. Please implement the printCard method (20 points, 10 for suit, 10 for rank)
 	public void printCard(){
 		//Hint: print (System.out.println) card as suit,rank, for example: print 1,1 as Clubs Ace
-
+		if(suit==1)
+			System.out.print("Clubs");
+		if(suit==2)
+			System.out.print("Diamonds");
+		if(suit==3)
+			System.out.print("Hearts");
+		if(suit==4)
+			System.out.print("Spades");
+		if(rank==1)
+			System.out.println("A");
+		if(rank==2)
+			System.out.println("2");
+		if(rank==3)
+			System.out.println("3");
+		if(rank==4)
+			System.out.println("4");
+		if(rank==5)
+			System.out.println("5");
+		if(rank==6)
+			System.out.println("6");
+		if(rank==7)
+			System.out.println("7");
+		if(rank==8)
+			System.out.println("8");
+		if(rank==9)
+			System.out.println("9");
+		if(rank==10)
+			System.out.println("10");
+		if(rank==11)
+			System.out.println("J");
+		if(rank==12)
+			System.out.println("Q");
+		if(rank==13)
+			System.out.println("K");
+		
 	}
 	public int getSuit(){
 		return suit;
